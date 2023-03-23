059a4b1535308a01761e24708cf16cd54eb61ecd
Achi <achikoogorgadze@gmail.com>
2023-03-23 21:06:36 +0400

    3 - api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -44,7 +44,7 @@ function App() {
 
       // save user to mongo database
       if (profileObj) {
-        const response = await fetch('http://localhost:8080/api/v1/users', {
+        const response = await fetch('https://codeprops.onrender.com/api/v1/users', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
@@ -109,7 +109,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://codeprops.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
